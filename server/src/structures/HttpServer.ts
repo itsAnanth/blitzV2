4913ea5979d7cb38458e21cbe393fab278c4377c
itsAnanth <danonymousarmy@gmai.com>
2023-03-26 13:59:14 +0530

    feat(`server:http server`)

---
+++ server/src/structures/HttpServer.ts
@@ -0,0 +1,63 @@
+import { readdirSync } from 'fs';
+import express from 'express';
+import type { Express } from 'express';
+import { HttpTypes } from '../../../shared/HTTP';
+import HttpEvent from './Events/HttpEvent';
+import { createServer } from 'http';
+import { httpTypeArray } from '../../../shared/HTTP/HttpTypes';
+import Logger from '../utils/Logger';
+import cors from 'cors';
+
+class HttpServer {
+    app: Express;
+    // { GET: { 'routepath': Event } }
+    events: Map<HttpTypes, Map<string, HttpEvent>>
+    server: ReturnType<typeof createServer>;
+    PORT: number;
+
+    constructor(PORT: number = 3000) {
+        this.PORT = PORT;
+        this.events = new Map();
+
+        for (let i = 0; i < httpTypeArray.length; i++)
+            this.events.set(i, new Map())
+    }
+
+    async init() {
+        const eventsPath = readdirSync('./src/events/HTTP');
+
+        if (eventsPath.length === 0)
+            return Logger.log("Empter server events");
+
+        for (let i = 0; i < eventsPath.length; i++) {
+            const event = (await import(`../events/HTTP/${eventsPath[i]}`)).default as HttpEvent;
+
+            const eventType = this.events.get(event.method);
+
+            if (eventType.has(event.route)) continue;
+
+            eventType.set(event.route, event);
+        }
+
+        Logger.log(`[HTTP_SERVER]`, this.events)
+    }
+
+    start() {
+        this.app = express();
+
+        for (const [method, event] of this.events.entries()) {
+            for (const [route, eventHandler] of event.entries()) {
+
+                const _method: 'get' | 'post' | 'put' | 'delete' = HttpTypes[method].toLowerCase() as any;
+
+                this.app[_method](route, eventHandler.callback.bind(this));
+            }
+        }
+
+        this.app.use(cors());
+        this.server = createServer(this.app);
+        this.server.listen(this.PORT, () => Logger.log(`[HTTP_SERVER] Listening on PORT ${this.PORT}`));
+    }
+}
+
+export default HttpServer;
